d03c837ad5116d2815127cfd4567bf796fec5035
Spelljinxer <reiden556@gmail.com>
2022-11-29 17:03:01 +0800

    idk

--- secretword.py
+++ secretword.py
@@ -10,13 +10,37 @@ with open("words.txt") as f:
     secretword = random.choice(f.readlines()).strip()
 
 class Game:
+    guess_count = 0
     round_count = 0 
-    
-    def __init__(self) -> None:
-        pass
+    difficulty = 0
 
-    def execute():
-        pass
+    def __init__(self, difficulty):
+        self.difficulty = int(difficulty)
+        if(self.difficulty) == 1:
+            self.guess_count = 25
+        elif(self.difficulty) == 2:
+            self.guess_count = 20
+        elif(self.difficulty) == 3:
+            self.guess_count = 15
+        elif(self.difficulty) == 4:
+            self.guess_count = 10
+        elif(self.difficulty) == 5:
+            self.guess_count = 5
+        
+    def check_win(self, guess):
+        return(guess == secretword)
+ 
+    def execute(self):
+        while(self.round_count < self.guess_count):
+            print("You have " + str(self.guess_count - self.round_count) + " guesses left.")
+            guess = input("Enter your guess: ")
+            if(self.check_win(guess)):
+                print("You won in " + str(self.round_count) + " guesses!")
+                break
+            self.round_count += 1
+        
+        print("You lost! The word was " + secretword + ".")
+
 
 #----------------- main func ----------------------
 def print_usage():
@@ -36,7 +60,9 @@ if __name__ == '__main__':
         print_usage()
         sys.exit(1)
     print("You have chosen to play on difficulty level " + sys.argv[2])
-    user_guess = input("Guess the secret word: ")
+    
+    Game = Game(sys.argv[2])
+    Game.execute()
 
 
     
